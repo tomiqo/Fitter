0f24d0a27bd66a160a900d0751a0d83ff17cbaf7
Snasi <snasel.snasel@seznam.cz>
2019-03-04 14:45:37 +0100

    Pokus o vytvoření správného respozitáře

--- ics_iw5_ProjDatabases.DAL/Class1.cs
+++ ics_iw5_ProjDatabases.DAL/Class1.cs
@@ -2,7 +2,10 @@
 
 namespace ics_iw5_ProjDatabases.DAL
 {
+    // DAL - Data Access Layer  
     public class Class1
     {
+        public int ID_pro { get; set; }
+        public string jmeno { get; set; }
     }
 }
