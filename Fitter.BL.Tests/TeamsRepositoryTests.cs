143eb0caa1abd78cd1dfeabb71d8c7ce69046579
Adrian <xboros03@stud.fit.vutbr.cz>
2019-04-07 00:36:48 +0200

    Tests does not work

---
+++ Fitter.BL.Tests/TeamsRepositoryTests.cs
@@ -0,0 +1,192 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.Runtime.InteropServices.ComTypes;
+using Fitter.BL.Model;
+using Fitter.BL.Repositories;
+using Fitter.BL.Repositories.Interfaces;
+using Fitter.DAL.Entity;
+using Fitter.DAL.Tests;
+using Xunit;
+
+namespace Fitter.BL.Tests
+{
+    public class TeamsRepositoryTests
+    {
+        [Fact]
+        public void CreateTeam()
+        {
+            var sut = CreateSUT();
+            var model = new TeamDetailModel()
+            {
+                Admin = new UserDetailModel()
+                {
+                    LastName = "Eva",
+                    FirstName = "Novakova",
+                    Password = "asassad",
+                    Email = "novakovaEva@gmail.com"
+                },
+                Name = "Team",
+                Description = "Team pre IW5/ICS",
+            };
+            var createdTeam = sut.Create(model);
+            Assert.NotNull(createdTeam);
+        }
+
+        [Fact]
+        public void GetTeamById()
+        {
+            var sut = CreateSUT();
+            var model = new TeamDetailModel()
+            {
+                Admin = new UserDetailModel()
+                {
+                    LastName = "Alfonz",
+                    FirstName = "Mokry",
+                    Password = "147258369",
+                    Email = "dark@gmail.com"
+                },
+                Name = "The bests",
+                Description = "Alfonzov team",
+            };
+            var createdTeam = sut.Create(model);
+            var foundedTeam = sut.GetById(createdTeam.Id);
+            Assert.NotNull(foundedTeam);
+        }
+
+        [Fact]
+        public void DeleteTeam()
+        {
+            var sut = CreateSUT();
+            var model = new TeamDetailModel()
+            {
+                Admin = new UserDetailModel()
+                {
+                    LastName = "Adam",
+                    FirstName = "Vysoky",
+                    Password = "adamko13213",
+                    Email = "andershall@gmail.com"
+                },
+                Name = "United",
+                Description = "Najlepsi futbalovy team",
+            };
+            var createdTeam = sut.Create(model);
+            sut.Delete(createdTeam.Id);
+            Assert.Null(createdTeam.Name);
+        }
+
+        [Fact]
+        public void AddUserToTeam()
+        {
+            var sut = CreateSUT();
+            var model = new TeamDetailModel()
+            {
+                Admin = new UserDetailModel()
+                {
+                    LastName = "Michaela",
+                    FirstName = "Sucha",
+                    Password = "miskasucha",
+                    Email = "michaella159@seznam.cz"
+                },
+                Name = "Miskin team",
+                Description = "Dievcensky team pre michaelu",
+            };
+            var user = new UserDetailModel()
+            {
+                LastName = "Eva",
+                FirstName = "Novakova",
+                Password = "asassad",
+                Email = "novakovaEva@gmail.com"
+            };
+            var createdTeam = sut.Create(model);
+            sut.AddUserToTeam(user,createdTeam.Id);
+            Assert.NotNull(user);
+        }
+
+        [Fact]
+        public void RemoveUserFromTeam()
+        {
+            var sut = CreateSUT();
+            var users = CreateUser();
+            var model = new TeamDetailModel()
+            {
+                Admin = new UserDetailModel()
+                {
+                    LastName = "Vaclav",
+                    FirstName = "Siroky",
+                    Password = "dobreheslo",
+                    Email = "sirokyvaclav@gmail.com"
+                },
+                Name = "Ministri",
+                Description = "Team pre ministrov",
+            };
+            var user = new UserDetailModel()
+            {
+                LastName = "Michal",
+                FirstName = "Kruty",
+                Password = "najlepsieheslo",
+                Email = "kruty@gmail.com"
+            };
+
+            var createdUser = users.Create(user);
+            var createdTeam = sut.Create(model);
+            sut.AddUserToTeam(user, createdTeam.Id);
+            sut.RemoveUserFromTeam(user, createdTeam.Id);
+            var usersInTeam = users.GetUsersInTeam(createdUser.Id);
+            Assert.Null(usersInTeam);
+        }
+
+        [Fact]
+        public void CheckTeamExisting()
+        {
+            var sut = CreateSUT();
+            var model = new TeamDetailModel()
+            {
+                Admin = new UserDetailModel()
+                {
+                    LastName = "Vaclav",
+                    FirstName = "Siroky",
+                    Password = "dobreheslo",
+                    Email = "sirokyvaclav@gmail.com"
+                },
+                Name = "Ministri",
+                Description = "Team pre ministrov",
+            };
+
+            var createdTeam = sut.Create(model);
+            var teamExisting = sut.Exists(createdTeam.Name);
+            Assert.True(teamExisting);
+        }
+
+        [Fact]
+        public void CheckTeamExistingAfterDelete()
+        {
+            var sut = CreateSUT();
+            var user = new UserDetailModel()
+            {
+                LastName = "Michal",
+                FirstName = "Kruty",
+                Password = "najlepsieheslo",
+                Email = "kruty@gmail.com"
+            };
+            var model = new TeamDetailModel()
+            {
+                Admin = user,
+                Name = "Michalov team",
+                Description = "Najlepsi z najlepsich",
+            };
+
+            var createdTeam = sut.Create(model);
+            sut.Delete(createdTeam.Id);
+            var exist = sut.Exists(createdTeam.Name);
+            Assert.False(exist);
+        }
+        private TeamsRepository CreateSUT()
+        {
+            return new TeamsRepository(new InMemoryFitterDbContext(), new Mapper.Mapper());
+        }
+
+        private UsersRepository CreateUser()
+        {
+            return new UsersRepository(new InMemoryFitterDbContext(), new Mapper.Mapper());
+        }
+    }
+}
--- Fitter.BL.Tests/UsersRepositoryTests.cs
+++ Fitter.BL.Tests/UsersRepositoryTests.cs
@@ -1,30 +1,65 @@
-﻿using Fitter.BL.Model;
+﻿using System;
+using System.Linq;
+using Fitter.BL.Model;
+using Fitter.BL.Repositories;
+using Fitter.BL.Repositories.Interfaces;
+using Fitter.DAL.Tests;
 using Xunit;
 
 namespace Fitter.BL.Tests
 {
-    public class UsersRepositoryTests : IClassFixture<FitterRepositoryTestsFixture>
+    public class UsersRepositoryTests
     {
-        private readonly FitterRepositoryTestsFixture fixture;
-
-        public UsersRepositoryTests(FitterRepositoryTestsFixture fixture)
-        {
-            this.fixture = fixture;
-        }
-
         [Fact]
         public void CreateUser()
         {
+            var sut = CreateSUT();
             var model = new UserDetailModel
             {
-                FirstName = "jozef",
-                LastName = "kovac",
-                Password = "asdfg"
+                FirstName = "Jozef",
+                LastName = "Kovac",
+                Password = "asdf",
+                Email = "kovacjozef@gmail.com"
             };
+            var createdUser = sut.Create(model);
+            Assert.NotNull(createdUser);
+        }
 
-            var returnedModel = fixture.Repository.Create(model);
+        [Fact]
+        public void GetUserById()
+        {
+            var sut = CreateSUT();
+            var model = new UserDetailModel()
+            {
+                FirstName = "Martin",
+                LastName = "Slovak",
+                Email = "martinko123@azet.sk",
+                Password = "assaf"
+            };
+            var createdUser = sut.Create(model);
+            var foundedUser = sut.GetById(createdUser.Id);
+            Assert.NotNull(foundedUser);
+        }
 
-            Assert.NotNull(returnedModel);
+        [Fact]
+        public void CheckUsersPassword()
+        {
+            var sut = CreateSUT();
+            var model = new UserDetailModel()
+            {
+                FirstName = "Ivana",
+                LastName = "Sucha",
+                Email = "ivanasucha@seznam.cz",
+                Password = "123456"
+            };
+            var createdUser = sut.Create(model);
+            var foundedUser = sut.GetByEmail(model.Email);
+            Assert.Equal(foundedUser.Password, model.Password);
+        }
+
+        private IUsersRepository CreateSUT()
+        {
+            return new UsersRepository(new InMemoryFitterDbContext(), new Mapper.Mapper());
         }
     }
 }
