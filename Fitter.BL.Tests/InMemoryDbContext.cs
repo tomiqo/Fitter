45fb5f3c4e0d82d1999401dba027c8d1773dd1e6
Adrian <xboros03@stud.fit.vutbr.cz>
2019-04-06 21:50:31 +0200

    Tests does not work

---
+++ Fitter.BL.Tests/InMemoryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Fitter.BL.Factories;
+using Fitter.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitter.BL.Tests
+{
+    public class InMemoryDbContext : IDbContextFactory
+    {
+        public FitterDbContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FitterDbContext>();
+            optionsBuilder.UseInMemoryDatabase("FitterDB");
+            return new FitterDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Fitter.BL.Tests/UsersRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Fitter.BL.Model;
+using Xunit;
+
+namespace Fitter.BL.Tests
+{
+    public class UsersRepositoryTests : IClassFixture<FitterRepositoryTestsFixture>
+    {
+        private readonly FitterRepositoryTestsFixture fixture;
+
+        public UsersRepositoryTests(FitterRepositoryTestsFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void CreateUser()
+        {
+            var model = new UserDetailModel
+            {
+                FirstName = "jozef",
+                LastName = "kovac",
+                Password = "asdfg"
+            };
+
+            var returnedModel = fixture.Repository.Create(model);
+
+            Assert.NotNull(returnedModel);
+        }
+    }
+}
