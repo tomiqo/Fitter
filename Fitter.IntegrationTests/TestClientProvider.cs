3063c7bead1bcb9060fd2bf77b81a8bcd781e409
Adrian <xboros03@stud.fit.vutbr.cz>
2019-04-10 00:16:16 +0200

    API tests in progress

---
+++ Fitter.IntegrationTests/TestClientProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using Fitter.Swagger.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Fitter.IntegrationTests
+{
+    public class TestClientProvider : IDisposable
+    {
+        private TestServer TestServer;
+        public HttpClient Client { get; private set; }
+        public TestClientProvider()
+        {
+            TestServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = TestServer.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            TestServer?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
---
+++ Fitter.IntegrationTests/UsersControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Fitter.BL.Model;
+using Fitter.DAL.Entity;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Fitter.IntegrationTests
+{
+    public class UsersControllerTests
+    {
+        [Fact]
+        public async Task GetUserByEmail()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/users/getByEmail?email=adrianboros@centrum.sk");
+
+                response.EnsureSuccessStatusCode();
+
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+
+        [Fact]
+        public async Task CreateUser()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var response = await client.PostAsync("/api/users/create", new StringContent(
+                   JsonConvert.SerializeObject(new UserDetailModel(){ Email = "adrianboros2@gmail.com", LastName = "Abraham",
+                    FirstName = "Adam", Password = "123", Id = Guid.NewGuid()}), Encoding.UTF8, "application/json"));
+
+                response.EnsureSuccessStatusCode();
+
+                response.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+        }
+    }
+}
