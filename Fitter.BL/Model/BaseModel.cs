b28ca4c53787d532afd664018299676dbe6bccd3
Tomáš Žigo <xzigot00@vutbr.cz>
2019-04-10 00:00:20 +0200

    Converter, Basic messages and initial mvvm communication

--- Fitter.BL/Model/BaseModel.cs
+++ Fitter.BL/Model/BaseModel.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Fitter.BL.Model
 {
-    public abstract class BaseModel
+    public abstract class BaseModel : INotifyPropertyChanged
     {
         public Guid Id { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        } 
     }
 }
