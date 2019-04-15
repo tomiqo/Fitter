27c647199d3665c0760973bf7a26150b83111073
Adam Abrahám <xabrah04@vutbr.cz>
2019-04-15 17:50:17 +0200

    API published, MainWindow ViewModel

--- Fitter.App/API/GenerateApiClient.ps1
+++ Fitter.App/API/GenerateApiClient.ps1
@@ -1,1 +1,1 @@
-autorest --csharp --fluent --namespace="Fitter.App.API" --override-client-name="APIClient" --sync-methods="all" --client-side-validation="true" --input-file="./swagger.json" --output-folder="./"
+autorest --csharp --fluent --namespace="Fitter.App.API" --override-client-name="APIClient" --sync-methods="all" --client-side-validation="true" --input-file="https://fitterapi.azurewebsites.net/swagger/v1/swagger.json" --output-folder="./"
---
+++ Fitter.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Fitter.App.Commands;
+using Fitter.App.ViewModels.Base;
+using Fitter.App.Views;
+using Fitter.BL.Messages;
+using Fitter.BL.Model;
+using Fitter.BL.Repositories.Interfaces;
+using Fitter.BL.Services;
+
+namespace Fitter.App.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand ExitApplication { get; set; }
+        public ICommand MinimizeApplication { get; set; }
+        public ICommand MaximizeApplication { get; set; }
+
+        public MainWindowViewModel()
+        {
+            
+            ExitApplication = new RelayCommand(Exit);
+            MinimizeApplication = new RelayCommand(Minimize);
+            MaximizeApplication = new RelayCommand(Maximize);
+        }
+
+        private void Exit()
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Minimize()
+        {
+            System.Windows.Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+        private void Maximize()
+        {
+            System.Windows.Application.Current.MainWindow.WindowState = WindowState.Maximized;
+        }
+    }
+}
